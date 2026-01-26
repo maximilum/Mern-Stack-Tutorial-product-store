e0926836332a97116ec3d8bfcc023195ddb0fe6a
Mohammed Zain <87908878+maximilum@users.noreply.github.com>
2026-01-26 14:24:43 +0200

    setup routes
    create routes and connect them to their respective components

---
+++ frontend/src/router/router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "../App";
+import CreateProduct from "../Components/CreateProduct";
+import Products from "../Components/Products";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Products></Products>,
+      },
+      {
+        path: "/createProduct",
+        element: <CreateProduct></CreateProduct>,
+      },
+    ],
+  },
+]);
+
+export default router;
